d8ad6976a54f77c99a544597e0d3081b509da117
simon <mpriverosb@libertadores.edu.co>
2023-09-03 18:52:19 -0500

    Initial commit

---
+++ app/src/main/java/MOVILES20232/ejemploui/MainActivity.java
@@ -0,0 +1,14 @@
+package MOVILES20232.ejemploui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_tabular);
+    }
+}
